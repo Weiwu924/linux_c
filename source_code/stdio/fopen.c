44596c3ce6e0e22d6e20dd9ae37412c3b2dff8c3
Weiwu924 <718069307@qq.com>
2023-08-30 03:55:18 +0800

    标准io

---
+++ source_code/stdio/fopen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main()
+{
+    FILE *fp;
+
+    fp = fopen("/tmp/out","r");
+
+    if(fp == NULL)
+    {
+        fprintf(stderr,"fopen() failed!,errno = %d\n",errno);
+        exit(1);
+    }   
+
+    puts("OK!");
+
+    exit(0);
+}
